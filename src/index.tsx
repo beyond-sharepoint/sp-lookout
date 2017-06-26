08c298faa0f0e1ce8f103ca57b1a92a473d1e080
Oceanswave <sean.mclellan@sixconcepts.com>
2017-06-26 12:57:45 -0400

    Import office-ui-fabric css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'office-ui-fabric-react/dist/css/fabric.css';
 import './index.css';
 
 ReactDOM.render(
