1faf9ad8c062660177a5d5c1f9103ebc2f4a1812
Oceanswave <sean.mclellan@sixconcepts.com>
2017-06-26 17:18:23 -0400

    some SCSS fixes; Throw top bar, left nav in.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import './App.css';
 
-const logo = require('./logo.svg');
+import { Fabric } from 'office-ui-fabric-react';
+import Workspace from './workspace/Workspace';
 
 class App extends React.Component<{}, {}> {
   public constructor(props: any) {
@@ -10,15 +11,9 @@ class App extends React.Component<{}, {}> {
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Fabric>
+        <Workspace />
+      </Fabric>
     );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'office-ui-fabric-react/dist/css/fabric.css';
+
 import './index.css';
+import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(
   <App />,
